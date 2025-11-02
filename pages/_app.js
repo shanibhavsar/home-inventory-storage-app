899ce54fa7fca439bc5943d793ecd6404297f2dc
shanibhavsar <shani.bhavsar@gmail.com>
2025-11-01 20:54:25 -0400

    Add default Google Sheet URL to localStorage

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,12 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const storedUsername = localStorage.getItem('username');
     const storedPassword = localStorage.getItem('password');
+    const storedSheetUrl = localStorage.getItem('sheetUrl');
+
     if (!storedUsername) localStorage.setItem('username', 'damn');
     if (!storedPassword) localStorage.setItem('password', 'damndamn');
+    if (!storedSheetUrl) localStorage.setItem('sheetUrl', 'https://docs.google.com/spreadsheets/d/1ENdVi-9QZgYM06_L5BSwJG6OvkaHGIq1FeBBGF2XYF0/edit');
+
     if (router.pathname !== '/login' && !isAuthenticated) {
       router.push('/login');
     }
